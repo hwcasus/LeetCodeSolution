b46ce74bbecaa1f1205316a3afc3bcac1ad967ba
hwcasus <hungwei_chen@asus.com>
2021-09-03 17:58:51 +0800

    Update

--- cs_notes/linked_list/linked_list_cycle_ii.py
+++ cs_notes/linked_list/linked_list_cycle_ii.py
@@ -8,6 +8,27 @@
 #         self.next = None
 
 class Solution(object):
+    def detectCycle(self, head: ListNode) -> ListNode:
+        # Revisit 20210730
+        if head is None or head.next is None:
+            return None
+
+        slow = head.next
+        fast = head.next.next
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return None
+            slow = slow.next
+            fast = fast.next.next
+
+        fast = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return fast
+
     def detectCycle(self, head):
         """
         :type head: ListNode
